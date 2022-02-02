b323d7e188214d879945d024b77eda11b4ffc637
burningfiremoon <33030929+burningfiremoon@users.noreply.github.com>
2022-02-01 23:11:34 -0800

    Screen finished

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-# create a screen
-# imports
-import pygame
-import random
+## download -m pip install -U pygame --user
 
+# imports
+import sys, pygame, random
+
+pygame.init()
+# -----FPS-----
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+# variables
+Game = True
+
+# colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# display/screen
+Display = pygame.display.set_mode((1920, 1080))
+Display.fill(BLACK)
+# main loop
+while True:
+
+    pygame.display.update()
+    if not Game:
+        pygame.quit()
+        sys.exit()
