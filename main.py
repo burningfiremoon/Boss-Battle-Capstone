1442c425f4f675990fc55f02059e1f2af588cc44
burningfiremoon <charleszhang4@gmail.com>
2022-02-03 12:43:10 -0800

    player class coded

--- main.py
+++ main.py
@@ -14,10 +14,40 @@ Game = True
 # colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
 # display/screen
 Display = pygame.display.set_mode((1920, 1080))
 Display.fill(BLACK)
+# ----- Classes ------
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        # what character looks like
+        self.image = pygame.image.load("./Sprites/right_player1-1.png")
+        # Hit box
+        self.rect = self.image.get_rect()
+
+        # ___Speed of the player___
+        self.vel_x = 0
+        self.vel_y = 0
+
+        # direction variables and HP
+        self.facing_right = True
+        self.hp = 3
+
+        # list of objects the sprites cannot walk through
+        self.level = None
+
+
+
+
+
+
+
 # main loop
 while True:
 
